12be326b496b364b4ebde30afc2fa57275575d12
Bartosz Smorawski <holzturbs@gmail.com>
2023-06-23 12:03:12 +0200

    set all done interaction

--- src/App.js
+++ src/App.js
@@ -37,6 +37,15 @@ function App() {
     );
   };
 
+  const setAllDone = () => {
+    setTasks((tasks) =>
+      tasks.map((task) => ({
+        ...task,
+        done: true,
+      }))
+    );
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -57,6 +66,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />

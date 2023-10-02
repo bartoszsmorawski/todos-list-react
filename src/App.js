bb4d4730053960a1b827eaa878685065047084f8
Bartosz Smorawski <holzturbs@gmail.com>
2023-10-02 11:19:50 +0200

    install react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
 import React from "react";
 import Tasks from "./features/tasks/Tasks";
+import {
+  BroserRouter,
+  Link,
+  Switch,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
-export default () => <Tasks />;
+export default () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <Link to="/zadania">Zadania</Link>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+      </Switch>
+    </nav>
+  </BrowserRouter>
+);
